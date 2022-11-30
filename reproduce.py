9aa9db0cf46c568e1a9d3a7e764e5968dd7ef391
pestrstr <giuseppestracquadanio2@gmail.com>
2022-12-01 00:40:06 +0100

    fix reproduce script

--- reproduce.py
+++ reproduce.py
@@ -7,27 +7,15 @@ description =  ''' Reproducing Code for CS421 Reproducibility Challenge.
 Authors: Giuseppe Stracquadanio & Giuseppe Concialdi.
 '''
 
-reproduce_parser = argparse.ArgumentParser(description=description)
-reproduce_parser.add_argument('--train', help='train LOVE model', action='store_true')
-reproduce_parser.add_argument('--eval', help='evaluate LOVE model', action='store_true')
-reproduce_parser.add_argument('--model_path', help='specify LOVE model path for eval', type=str, default='./output/model_20.pt')
-reproduce_parser.add_argument('--eval_all', help='evalaute LOVE models (all epochs) in one shot. requires complete training', action='store_true')
-
-try:
-    reproduce_args = reproduce_parser.parse_args()
-except:
-    reproduce_parser.print_help()
-    sys.exit(0)
-
 def train():
     from train import main as trainLOVE
     trainLOVE()
 
-def eval():
+def eval(args):
     TOKENIZER = tokenization.FullTokenizer(vocab_file='data/vocab.txt', do_lower_case=True)
     from evaluate import overall
     from train import args as train_args
-    overall(train_args, reproduce_parser.model_path, TOKENIZER)
+    overall(train_args, args.model_path, TOKENIZER)
 
 def eval_all():
     from evaluate_models import evaluate
@@ -35,10 +23,23 @@ def eval_all():
     evaluate(train_args)
 
 def main():
+    reproduce_parser = argparse.ArgumentParser(description=description)
+    reproduce_parser.add_argument('--train', help='train LOVE model', action='store_true')
+    reproduce_parser.add_argument('--eval', help='evaluate LOVE model', action='store_true')
+    reproduce_parser.add_argument('--model_path', help='specify LOVE model path for eval', type=str, default='./output/model_20.pt')
+    reproduce_parser.add_argument('--eval_all', help='evalaute LOVE models (all epochs) in one shot. requires complete training', action='store_true')
+    reproduce_parser.add_argument('--test', help='evalaute LOVE models (all epochs) in one shot. requires complete training', action='store_true')
+
+    try:
+        reproduce_args = reproduce_parser.parse_args()
+    except:
+        reproduce_parser.print_help()
+        sys.exit(0)
+
     if reproduce_args.train:
         train()
     elif reproduce_args.eval:
-        eval()
+        eval(reproduce_args)
     elif reproduce_args.eval_all:
         eval_all()
         
